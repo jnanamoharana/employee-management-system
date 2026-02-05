fda94851fcf4c2e83d83c49dbdfd06a2555d8ad6
DELL <DELL@DESKTOP-GKC70AM>
2026-02-05 22:01:25 +0530

    DTO validation added

--- src/main/java/com/jnana/employeemanagement/entity/Employee.java
+++ src/main/java/com/jnana/employeemanagement/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.jnana.employeemanagement.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,7 +14,9 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false)
 	private String name;
+	@Column(nullable = false,unique=true)
 	private String email;
 	private String dept;
 	public Long getId() {
